a36fcdc931cd1632503af95b80f80e950dacb9b9
0918nobita <nobita.0918@gmail.com>
2018-11-09 17:03:23 +0900

    :pencil: クロージャを用いたサンプルを追加した

--- example.lisp
+++ example.lisp
@@ -4,4 +4,20 @@
 
 (labels ((foo (x) (* x 2)))
   (print (foo 2))
-  (print (foo 3)))  ; 2, 3
+  (print (foo 3)))  ; 4, 6
+
+#|
+(defvar count 0)
+(defun counter () (print (incf count)))
+(defun reset-count () (print (setf count 0)))
+|#
+
+(let ((cnt 0))
+  (defun counter () (print (incf cnt)))
+  (defun reset-count () (print (setf cnt 0))))
+
+(counter) ; 1
+(counter) ; 2
+(reset-count) ; 0
+(counter) ; 1
+(counter) ; 2
