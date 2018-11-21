a444fb698664c9db8d64cf9b6af14bfe490b8a6b
0918nobita <nobita.0918@gmail.com>
2018-11-21 20:46:10 +0900

    :sparkles: 平行四辺形の面積を求める parallelogram 関数を実装した

--- example2.lisp
+++ example2.lisp
@@ -1,9 +1,4 @@
-(setf a '((1 2 3) (1 2 3) (1 2 3)))
-(setf b '((2 3 4) (2 3 4) (2 3 4) (2 3 4 5)))
+; 平行四辺形の面積 (底辺, 高さ)
+(defun parallelogram (base height) (* base height))
 
-; 渡した多重リストが、行列として正しい形式かどうか
-(defun validation (list)
-  (equal 1 (length (remove-duplicates (mapcar #'length list)))))
 
-(print (validation a)) ; T
-(print (validation b)) ; NIL
