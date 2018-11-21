1ea85432a31b38ccbada7e83e1f41681f6bbfbc9
0918nobita <nobita.0918@gmail.com>
2018-11-21 20:52:05 +0900

    :sparkles: リストの先頭要素を変更する updateHead 関数を実装した

--- example2.lisp
+++ example2.lisp
@@ -6,3 +6,13 @@
 
 ; 三角錐の体積 (底面積, 高さ)
 (defun volume (s h) (/ (* s h) 3))
+
+(defvar a '(1 2 3))
+(setf (car a) 10) ; 先頭要素を 10 に変更
+(setf (car a) 1)  ; 先頭要素を 1 に変更
+
+; 先頭要素を変更する
+(defun updateHead (list element) (setf (car list) element))
+
+(updateHead a 10)
+(print a)  ; (10 2 3)
